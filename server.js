8596a14495cb48fd2c621b16eafb10e21e750351
Spew13 <carmstrong31@ecsd.us>
2025-10-02 14:17:51 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.get('/', (req, res) => {
+  res.send('Spew13 Socket.IO Server running');
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('adminCommand', (data) => {
+    io.emit('executeCommand', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
